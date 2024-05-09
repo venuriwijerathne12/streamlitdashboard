5c161a54a5ab52bc8016dbbffe16ce809f82fa8a
venuriwijerathne12 <168188591+venuriwijerathne12@users.noreply.github.com>
2024-05-09 14:10:58 +0530

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,7 +5,7 @@ import os
 import warnings
 warnings.filterwarnings('ignore')
 
-st.set_page_config(page_title="Superstore!!!", page_icon=":bar_chart:",layout="wide")
+st.set_page_config(page_title="Superstore", page_icon=":bar_chart:",layout="wide")
 
 st.title(" :bar_chart: Sample SuperStore EDA")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>',unsafe_allow_html=True)
